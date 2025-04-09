f6071432218a1bbe2afac5d9e64864741c216803
root <root@localhost.localdomain>
2025-04-09 06:21:57 -0500

    second

--- web3genr8-version-specific/docker-26.1.4/provision/mysql/init/01-databases.sql
+++
@@ -1,8 +0,0 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `primary`;
-CREATE DATABASE IF NOT EXISTS `secondary`;
-
-# create root user and grant rights
-CREATE USER 'local1'@'localhost' IDENTIFIED BY 'local';
-GRANT ALL PRIVILEGES ON *.* TO 'local1'@'%' identified by 'local';
-FLUSH PRIVILEGES;
